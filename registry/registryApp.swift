7731f167cd7b4b2fb1004dba29637248aa5f0924
Mitch Rogers <mitchellmel@outlook.com>
2023-03-16 10:18:05 -0500

    init

---
+++ registry/registryApp.swift
@@ -0,0 +1,17 @@
+//
+//  registryApp.swift
+//  registry
+//
+//  Created by Mitch Rogers on 3/16/23.
+//
+
+import SwiftUI
+
+@main
+struct registryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
